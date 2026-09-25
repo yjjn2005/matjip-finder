320337f871abfd5227058496c66fd3468e1151f0
yjjn2005 <yjjn2005@gmail.com>
2026-09-25 04:34:52 +0000

    Cloudflare Worker 동기화 연결 (matjip-finder-api)

--- worker/worker.js
+++ worker/worker.js
@@ -1,4 +1,3 @@
-// Cloudflare Worker: PIN 기반 맛집 노트 동기화 (KV: MATJIP_SYNC)
 async function sha256(t){
   const b=await crypto.subtle.digest("SHA-256",new TextEncoder().encode(t));
   return [...new Uint8Array(b)].map(x=>x.toString(16).padStart(2,"0")).join("");
